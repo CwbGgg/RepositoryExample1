638fbbaa9443b33b13937404186f587343875dbc
chenwenbin <948348906@qq.com>
2019-07-22 15:33:09 +0800

    删除这个OA

--- oa/oa_biz/src/main/java/com/imooc/oa/biz/GlobalBiz.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.oa.biz;
-
-import com.imooc.oa.entity.Employee;
-
-public interface GlobalBiz {
-   public Employee login(String sn,String password);
-   public void changePassword(Employee employee);
-}
